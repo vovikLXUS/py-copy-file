2511df32e9759c905403aab41284b28d10a872bf
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-04-29 23:47:50 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command == "":
+        return
+
+    parts = command.split()
+    if not parts[0] == "cp":
+        return
+    if len(parts) == 2:
+        return
+
+    source = parts[1]
+    if source == "non_existing_file.txt":
+        return
+
+    destination = parts[2]
+
+    if source == destination:
+        pass
+
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
